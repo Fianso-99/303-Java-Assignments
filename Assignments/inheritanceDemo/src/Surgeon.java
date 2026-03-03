64efd1773665282788e8d0d5e8eacba4ecc639f3
Fianso-99 <kenioua.soufiane@gmail.com>
2026-03-03 11:18:52 -0500

    Add files via upload

---
+++ Assignments/inheritanceDemo/src/Surgeon.java
@@ -0,0 +1,8 @@
+public class Surgeon extends Doctor {
+
+    void Surgeon_Details() {
+        System.out.println("Surgeon Detail...");
+        Department = "Cardio";
+        System.out.println(Department);
+    }
+}
